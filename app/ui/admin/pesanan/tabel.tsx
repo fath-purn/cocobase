7e1e0b54cde8c7122982ddd69bbf86155b9b9d0a
Fatkhurrohaman Purnomo <talitakamilia345@gmail.com>
2025-01-25 22:32:32 +0700

    fix: tabble pesanan gada scroll

--- app/ui/admin/pesanan/tabel.tsx
+++ app/ui/admin/pesanan/tabel.tsx
@@ -1,10 +1,6 @@
 "use client";
 import Icon from "@mdi/react";
-import {
-  mdiDeleteOutline,
-  mdiExpandAll,
-  mdiRefresh,
-} from "@mdi/js";
+import { mdiDeleteOutline, mdiExpandAll, mdiRefresh } from "@mdi/js";
 import { Pembeli, ProdukSelect, Transaksi } from "@/app/utils/interface";
 import { getData } from "@/app/utils/fetchData";
 import { useState, useEffect } from "react";
@@ -86,8 +82,8 @@ export default function Table({
 
   const handleRefresh = () => {
     toast.info("Refresh Data");
-    setRefresh(!refresh)
-  }
+    setRefresh(!refresh);
+  };
 
   return (
     <div>
@@ -166,8 +162,9 @@ export default function Table({
             className="fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-75 flex justify-center items-center z-50"
             onClick={() => setShowModal(false)}
           >
+            {/* jika pengen scroll tabel doang bisa diganti yang max-h-screen jadi h-screen */}
             <div
-              className="bg-white p-4 rounded-lg w-2/3 h -1/2 overflow-y-auto z-50"
+              className="bg-white p-4 rounded-lg w-2/3 max-h-screen overflow-y-auto z-50"
               onClick={(e) => e.stopPropagation()}
             >
               <h2 className="text-xl font-semibold mb-2">Transaksi</h2>
@@ -182,86 +179,87 @@ export default function Table({
                   <p>: {selectedPembeli?.no_telp}</p>
                   <p>: {selectedPembeli?.alamat}</p>
                 </div>
+                <div className="flex gap-3 justify-end col-span-2">
+                  {selectedPembeli && produk && (
+                    <FormTransaksi
+                      idPembeli={selectedPembeli.id}
+                      namaPembeli={selectedPembeli.nama}
+                      produk={produk}
+                    />
+                  )}
+
+                  {!selectedPembeli ||
+                    (!produk && <p>Refres untuk update data</p>)}
+
+                  <button
+                    className="flex h-10 rounded-lg bg-gray-600 mr-1 items-center p-3 text-gray-200 font-semibold transition-colors hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
+                    onClick={() => setShowModal(!showModal)}
+                  >
+                    Close
+                  </button>
+                </div>
               </div>
 
-              <div className="flex flex-col items-center mt-4">
-                <table className="w-full border-collapse border border-gray-300">
-                  <thead>
-                    <tr className="bg-gray-100 grid grid-cols-12">
-                      <th className="border border-gray-300 p-2 col-span-1">
-                        No
-                      </th>
-                      <th className="border border-gray-300 p-2 col-span-3">
-                        Produk
-                      </th>
-                      <th className="border border-gray-300 p-2 col-span-1">
-                        Jumlah
-                      </th>
-                      <th className="border border-gray-300 p-2 col-span-2">
-                        Harga
-                      </th>
-                      <th className="border border-gray-300 p-2 col-span-3">
-                        Total
-                      </th>
-                      <th className="border border-gray-300 p-2 col-span-2">
-                        Tanggal
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {selectedPembeli?.transaksi.map(
-                      (transaksi: Transaksi, index: number) => (
-                        <tr
-                          key={transaksi.id}
-                          style={{
-                            backgroundColor:
-                              index % 2 === 1 ? "#f2f2f2" : "#ffffff",
-                          }}
-                          className="bg-gray-100 grid grid-cols-12"
-                        >
-                          <td className="border border-gray-300 p-[6px] text-center col-span-1">
-                            {index + 1}
-                          </td>
-                          <td className="border border-gray-300 p-[6px] col-span-3">
-                            {transaksi.produk}
-                          </td>
-                          <td className="border border-gray-300 p-[6px] text-center col-span-1">
-                            {transaksi.jumlah}
-                          </td>
-                          <td className="border border-gray-300 p-[6px] col-span-2">
-                            Rp {transaksi.harga}
-                          </td>
-                          <td className="border border-gray-300 p-[6px] col-span-3">
-                            Rp {transaksi.total}
-                          </td>
-                          <td className="border border-gray-300 p-[6px] col-span-2">
-                            {transaksi.createdAt}
-                          </td>
-                        </tr>
-                      )
-                    )}
-                  </tbody>
-                </table>
-              </div>
-
-              <div className="flex mt-4 gap-3 justify-end">
-                {selectedPembeli && produk && (
-                  <FormTransaksi
-                    idPembeli={selectedPembeli.id}
-                    namaPembeli={selectedPembeli.nama}
-                    produk={produk}
-                  />
-                )}
-
-                {!selectedPembeli ||
-                  (!produk && <p>Refres untuk update data</p>)}
-
-                <button
-                  className="flex h-10 rounded-lg bg-gray-600 mr-1 items-center p-3 text-gray-200 font-semibold transition-colors hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
-                  onClick={() => setShowModal(!showModal)}
-                >
-                  Close
-                </button>
+              <div className="flex flex-col items-center mt-4 w-full max-h-[80%]">
+                <div className="overflow-y-auto max-h-full">
+                  <table className="w-full border-collapse border border-gray-300">
+                    <thead>
+                      <tr className="bg-gray-100 grid grid-cols-12">
+                        <th className="border border-gray-300 p-2 col-span-1">
+                          No
+                        </th>
+                        <th className="border border-gray-300 p-2 col-span-3">
+                          Produk
+                        </th>
+                        <th className="border border-gray-300 p-2 col-span-1">
+                          Jumlah
+                        </th>
+                        <th className="border border-gray-300 p-2 col-span-2">
+                          Harga
+                        </th>
+                        <th className="border border-gray-300 p-2 col-span-3">
+                          Total
+                        </th>
+                        <th className="border border-gray-300 p-2 col-span-2">
+                          Tanggal
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {selectedPembeli?.transaksi.map(
+                        (transaksi: Transaksi, index: number) => (
+                          <tr
+                            key={transaksi.id}
+                            style={{
+                              backgroundColor:
+                                index % 2 === 1 ? "#f2f2f2" : "#ffffff",
+                            }}
+                            className="bg-gray-100 grid grid-cols-12"
+                          >
+                            <td className="border border-gray-300 p-[6px] text-center col-span-1">
+                              {index + 1}
+                            </td>
+                            <td className="border border-gray-300 p-[6px] col-span-3">
+                              {transaksi.produk}
+                            </td>
+                            <td className="border border-gray-300 p-[6px] text-center col-span-1">
+                              {transaksi.jumlah}
+                            </td>
+                            <td className="border border-gray-300 p-[6px] col-span-2">
+                              Rp {transaksi.harga}
+                            </td>
+                            <td className="border border-gray-300 p-[6px] col-span-3">
+                              Rp {transaksi.total}
+                            </td>
+                            <td className="border border-gray-300 p-[6px] col-span-2">
+                              {transaksi.createdAt}
+                            </td>
+                          </tr>
+                        )
+                      )}
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
