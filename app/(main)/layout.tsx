24d779bbce0684025bb6e314eeb9590922922afd
Fatkhurrohaman Purnomo <talitakamilia345@gmail.com>
2025-02-07 14:01:46 +0700

    add: SEO

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { poppins } from '@/app/ui/fonts';
 import '../globals.css';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from '@/app/ui/navbar';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: {
@@ -20,9 +21,37 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="sitemap" href="/sitemap.xml" />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="google" content="notranslate" />
+        <link rel="canonical" href="https://cocobase-beta.vercel.app/" />
+        <meta name="author" content="Cocobase" />
+        <title>Cocobase</title>
+        <meta name="description" content="Cocobase adalah platform gula kelapa gula aren yang berkualitas" />
+        <meta name="keywords" content="Cocobase, gula kelapa, gula aren, gula kelapa aren, kelapa, cilongok, gula kelapa cilongok" />
+        <meta name="author" content="Cocobase" />
+        <meta property="og:title" content="Cocobase" />
+        <meta property="og:description" content="Cocobase adalah platform gula kelapa gula aren yang berkualitas" />
+        <meta property="og:url" content="https://cocobase-beta.vercel.app/" />
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "name": "Cocobase",
+              "url": "https://cocobase-beta.vercel.app/"
+            }
+          `}
+        </script>
+      </Head>
+
+
       <body className={`${poppins.className} bg-white text-[#202224] scroll-smooth`}>
         <Navbar />
         <div className="relative md:top-[48px] bottom-[48px] md:bottom-0">
+
           {children}
         </div>
       </body>
